92b0e610d255131368805a29dbc7d9c4500646eb
Victor Marks <vjmarks@gmail.com>
2019-07-02 23:48:19 -0700

    refactored code

--- app.js
+++ app.js
@@ -3,32 +3,30 @@ let boxNumber = 0;
 
 function startBox(evt) {
   evt.preventDefault();
-  console.log('start box');
+  console.log('start number of boxes');
   boxContainer.innerHTML = '';
   let selection = document.querySelector('.selection');
   let startNumber = selection.options[selection.selectedIndex].value;
-  console.log('selection: ', selection);
-  console.log('start: ', startNumber);
   boxNumber = 0;
   for (let i = 0; i < startNumber; i++) {
     addBox(boxNumber);
   }
 }
 
-function addBox(idx) {
-  console.log('adding box');
+function addBox() {
+  console.log('adding a box');
   let newBox = document.createElement('div');
   newBox.setAttribute('class', 'box');
   newBox.setAttribute('id', `${boxNumber}`);
   newBox.innerHTML = `
-    <button class="cancel-btn" onclick="removeBox(${boxNumber})">X</button><p>${boxNumber}</p>
+  <p>${boxNumber}</p><button class="cancel-btn" onclick="removeBox(${boxNumber})">X</button>
   `;
   boxContainer.appendChild(newBox);
   boxNumber++;
 }
 
 function removeBox(idx) {
-  console.log('removing box');
+  console.log('removing a box');
   let removeBox = document.getElementById(`${idx}`);
   let removed = boxContainer.removeChild(removeBox);
 }
